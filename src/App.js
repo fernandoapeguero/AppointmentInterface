948b7f95bb2c3ec9abd879b50a8e04a591ec88fe
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-27 23:23:06 -0400

    Add list to display appointment in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import { BiTrash } from "react-icons/bi";
+import { BiHealth, BiTrash } from "react-icons/bi";
+import AddAppointment from "./components/AddAppoinment";
+import Search from "./components/Search";
+import appointmentList from "./data.json";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl font-bold p-5">
         {" "}
-        <BiTrash className="inline-block text-red-200 align-top" /> Your
+        <BiHealth className="inline-block text-red-200 align-top" /> Your
         Appointments
       </h1>
+
+      <AddAppointment />
+      <Search />
+
+      <ul className="divided-y divide-gray-200 ">
+        {appointmentList.map((appointment) => (
+          <li className="px-3 py-3 flex items-start">
+            <button
+              type="button"
+              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              <BiTrash />
+            </button>
+            <div className="flex-grow">
+              <div className="flex items-center">
+                <span className="flex-none font-medium text-2xl text-blue-500">
+                  {appointment.petName}
+                </span>
+                <span className="flex-grow text-right">
+                  {appointment.aptDate}
+                </span>
+              </div>
+              <div>
+                <b className="font-bold text-blue-500">Owner:</b>{" "}
+                {appointment.ownerName}
+              </div>
+              <div className="leading-tight">{appointment.aptNotes}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
