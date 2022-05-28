41771a725ab2ec6c461c6a1a2c374f0c5d89381d
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-27 23:31:06 -0400

    Created component for appointment list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BiHealth, BiTrash } from "react-icons/bi";
 import AddAppointment from "./components/AddAppoinment";
+import AppointmentInfo from "./components/AppointmentInfo";
 import Search from "./components/Search";
 import appointmentList from "./data.json";
 
@@ -17,29 +18,7 @@ function App() {
 
       <ul className="divided-y divide-gray-200 ">
         {appointmentList.map((appointment) => (
-          <li className="px-3 py-3 flex items-start">
-            <button
-              type="button"
-              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <BiTrash />
-            </button>
-            <div className="flex-grow">
-              <div className="flex items-center">
-                <span className="flex-none font-medium text-2xl text-blue-500">
-                  {appointment.petName}
-                </span>
-                <span className="flex-grow text-right">
-                  {appointment.aptDate}
-                </span>
-              </div>
-              <div>
-                <b className="font-bold text-blue-500">Owner:</b>{" "}
-                {appointment.ownerName}
-              </div>
-              <div className="leading-tight">{appointment.aptNotes}</div>
-            </div>
-          </li>
+          <AppointmentInfo appointment={appointment} />
         ))}
       </ul>
     </div>
