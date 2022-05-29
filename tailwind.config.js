d5be43db76321d513d7b8bc3970ffe6a43e76806
fernandoapeguero <fernandoapeguero@gmail.com>
2022-05-29 11:22:09 -0400

    Updates

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  plugins: [require("@tailwindcss/forms")],
-};
